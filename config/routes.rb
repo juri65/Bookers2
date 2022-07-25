d8689541cd00f17e0d38059a16ccb2fb554dd4ce
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-07-25 02:29:41 +0000

    [Add−２3]

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
-  resources :users, only: [:show, :edit, :update
-]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about' => 'homes#about',as: 'about'
 end
