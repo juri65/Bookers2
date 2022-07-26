dfdcfad94eac7bdd87fc8d1715e7d9588d621fb5
EC2 Default User <106951993+juri65@users.noreply.github.com>
2022-07-26 08:31:48 +0000

    [Add−35]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
      flash[:notice] = "You have updated user successfully."
    redirect_to user_path(@user.id)
    else
-    render :index
+    render :edit
    end
   end 
    
